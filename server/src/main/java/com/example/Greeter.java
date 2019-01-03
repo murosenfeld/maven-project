cef0c5efe1f4da4e8a3d4a65a6f04a05d1f6bc71
Martin <murosenfeld@users.noreply.github.com>
2019-01-03 15:52:23 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
+/**
+*@param some
+*@return ddd
+*/
 
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
